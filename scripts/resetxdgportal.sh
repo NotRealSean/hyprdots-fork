a095b33942baa765a03d347cdf7c7b66bc7bda68
NotRealSean <seankungzaza@gmail.com>
2024-03-02 16:25:25 +0700

    Stop nuke script instead

--- scripts/resetxdgportal.sh
+++ scripts/resetxdgportal.sh
@@ -7,7 +7,7 @@ killall xdg-desktop-portal-lxqt
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 sleep 1
-#/usr/lib/xdg-desktop-portal-wlr &
-systemctl --user stop --now xdg-desktop-portal-wlr
+/usr/lib/xdg-desktop-portal-wlr &
+#systemctl --user stop --now xdg-desktop-portal-wlr
 sleep 2
 /usr/lib/xdg-desktop-portal &
