70ce62f84624113edf939dcc694b817ed8a3f737
NotRealSean <seankungzaza@gmail.com>
2024-03-02 15:09:07 +0700

    Change some keybinds, Adjust windowrules for xwayland stuff, Stop xdg-desktop-portal-wlr at launch

--- scripts/resetxdgportal.sh
+++ scripts/resetxdgportal.sh
@@ -7,6 +7,7 @@ killall xdg-desktop-portal-lxqt
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 sleep 1
-/usr/lib/xdg-desktop-portal-hyprland &
+#/usr/lib/xdg-desktop-portal-wlr &
+systemctl --user stop xdg-desktop-portal-wlr
 sleep 2
 /usr/lib/xdg-desktop-portal &
