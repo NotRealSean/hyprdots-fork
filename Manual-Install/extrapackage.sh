334db229c0d0752d552e7edc1bd8792629197666
NotRealSean <seankungzaza@gmail.com>
2024-07-15 14:09:19 +0700

    Add more pkg

--- Manual-Install/extrapackage.sh
+++ Manual-Install/extrapackage.sh
@@ -2,7 +2,7 @@
 
 echo "The following package will be install: zoxide eza atuin thefuck"
 read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-yay -S zoxide eza atuin thefuck
+yay -S zoxide eza atuin thefuck ttf-mononoki-nerd starship lazygit github-cli
 wait -n
 echo ""
 echo "Package installed, Please reopen the foot terminal"
