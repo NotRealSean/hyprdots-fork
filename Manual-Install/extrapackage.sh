6a5b5e874997b81be451d072101019c4e27099e8
NotRealSean <seankungzaza@gmail.com>
2024-07-17 13:51:06 +0700

    Added more package and script

--- Manual-Install/extrapackage.sh
+++ Manual-Install/extrapackage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "The following package will be install: zoxide eza atuin thefuck"
+echo "The following package will be install: zoxide eza atuin thefuck lazygit github-cli obs-studio vesktop-bin"
 read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-yay -S zoxide eza atuin thefuck ttf-mononoki-nerd starship lazygit github-cli
+yay -S zoxide eza atuin thefuck ttf-mononoki-nerd starship lazygit github-cli obs-studio vesktop-bin
 wait -n
 echo ""
 echo "Package installed, Please reopen the foot terminal"
---
+++ Manual-Install/installlazyvim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+nvim
