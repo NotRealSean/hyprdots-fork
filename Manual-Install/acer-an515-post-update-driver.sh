8e15e1fd0c853f07b58a5b2b6167fd507c7f1c6f
NotRealSean <seankungzaza@gmail.com>
2024-08-07 18:29:08 +0700

    added service install

--- Manual-Install/acer-an515-post-update-driver.sh
+++ Manual-Install/acer-an515-post-update-driver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo ./acer-predator-turbo-and-rgb-keyboard-linux-module/install.sh
+sudo ./acer-predator-turbo-and-rgb-keyboard-linux-module/install_service.sh
 
 cd ./Quanta-HD-User-Facing-0x0408-0x4035_linux && sudo ./install_driver.sh
 cd ..
